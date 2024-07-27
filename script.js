b83b02704f3923b85dd558cf523a63cb3f1ef783
Sunny Jate <sunnyjate2007@gmail.com>
2024-07-27 20:33:53 +0530

    Script of dayNight effect and content transition

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ let banner = document.querySelector(".banner");
 dayNight.addEventListener("click",()=>{
     banner.classList.toggle("night");
 })
+
+let typeEffect = new Typed("#text",{
+    strings:["Sunny G Jate","Coder","Developer"],
+    loop: true,
+    typeSpeed:60,
+    backSpeed:40,
+    backDelay:1000,
+})
